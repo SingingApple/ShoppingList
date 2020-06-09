44bf1c6a730c968d2909ff7c275d8b4498ed4655
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-06-10 00:57:55 +0530

    Version 1

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const items = require("./routes/api/items");
 const app = express();
+const path = require("path");
 
 //BodyParser Middleware
 app.use(bodyParser.json());
@@ -19,6 +20,15 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/items", items);
+
+if (process.env.NODE_ENV === "production") {
+  //Set Static Folder
+  app.use(express.static("/client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.port || 5000;
 
